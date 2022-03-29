598073233545fb4f14cbb13de0b4ceba787fecee
Jose-Alves-Neto <jose.alves.neto@cc.ufcg.edu.br>
2022-03-29 19:23:22 -0300

    commit funcionando

--- prolog/src/Storage.pl
+++ prolog/src/Storage.pl
@@ -1,22 +1,27 @@
+:- module(storage, []).
+
 :- use_module(library(csv)).
 
+%Verify the existence of a row in the knowledge base.
 row(N):- row(N,_,_,_,_).
 
 %Reads the CSV file and returns a list of rules.
-read_storage(File):-
-    csv_read_file(File, Rows),
+read_storage:-
+    csv_read_file('E:\\Developer\\estoque-console-app\\prolog\\storage-example.csv', Rows),
     assert_storage(Rows).
 
-%Asserts the rules in the knowledge db.
-assert_storage(Rows):-
-    maplist(assertz, Rows).
-
+%Writes the rules in the CSV file.
 write_storage(File):-
     condese_rows(0, Rows),
     csv_write_file(File, Rows).
 
+%Writes the rules in the knowledge base.
+assert_storage(Rows):-
+    maplist(assertz, Rows).
+
+%Condenses the rows of the knowledge base into a list.
 condese_rows(N, Rows):- call(row(N)),
-    findall(row(Id,Nome,Quantidade,Preco,Data),row(Id,Nome,Quantidade,Preco,Data), Rows).
+    findall(row(Id,Nome,Quantidade,Preco,Data),row(Id,Nome,Quantidade,Preco,Data),Rows).
 
 update_uid(Produtos, ID, NovosProdutos):- not(call(row(N))).
 update_uid(Produtos, ID, NovosProdutos).
