a4ad280810b6f55cc9a14bc6ce0e48fa20c5ba6b
Jose-Alves-Neto <jose.alves.neto@cc.ufcg.edu.br>
2022-03-29 17:41:37 -0300

    Storage, write and read csv files

---
+++ prolog/src/Storage.pl
@@ -0,0 +1,33 @@
+:- use_module(library(csv)).
+
+row(N):- row(N,_,_,_,_).
+
+%Reads the CSV file and returns a list of rules.
+read_storage(File):-
+    csv_read_file(File, Rows),
+    assert_storage(Rows).
+
+%Asserts the rules in the knowledge db.
+assert_storage([]).
+assert_storage(Rows):-
+    [Head|Tail] = Rows,
+    assertz(Head),
+    assert_storage(Tail).
+
+write_storage(File):-
+    condese_rows(0, Rows),
+    csv_write_file(File, Rows).
+
+condese_rows(N, Rows):- call(row(N)),
+    findall(row(Id,Nome,Quantidade,Preco,Data),row(Id,Nome,Quantidade,Preco,Data), Rows).
+
+update_uid(Produtos, ID, NovosProdutos):- not(call(row(N))).
+update_uid(Produtos, ID, NovosProdutos).
+
+update_uid_aux(Produtos, ID, NovosProdutos).
+
+verify_storage(Produtos, ProdutosVazios).
+
+verify_validade_produto(Produto, ProdutoValido).
+
+verify_validade_estoque(Produto, Data, EstoqueValido).
